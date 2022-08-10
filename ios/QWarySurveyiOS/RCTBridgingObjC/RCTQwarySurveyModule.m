516498f33bd583ca6b1d8913787641ba4d61af71
Fahid Attique <fahad@techmatetech.com>
2022-08-10 14:25:21 +0500

    runSurvey() function added and module name is set as Survey.

--- ios/QWarySurveyiOS/RCTBridgingObjC/RCTQwarySurveyModule.m
+++ ios/QWarySurveyiOS/RCTBridgingObjC/RCTQwarySurveyModule.m
@@ -7,15 +7,24 @@
 
 #import "RCTQwarySurveyModule.h"
 #import <React/RCTLog.h>
+#import "qwary-Swift.h"
 
 @implementation RCTQwarySurveyModule
 
 // To export a module named RCTCalendarModule
-RCT_EXPORT_MODULE();
+RCT_EXPORT_MODULE(Survey);
 
-RCT_EXPORT_METHOD(showQwarySurvey:(NSString *)name location:(NSString *)location)
+RCT_EXPORT_METHOD(runSurvey:(NSString *)message callback:(RCTResponseSenderBlock)cb)
 {
-  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+  RCTLogInfo(@"runSurvey method called in iOS with message: %@", message);
+  
+  QWSurveyRequest * request = [[QWSurveyRequest alloc] initWithScheme:@"https" host:@"survey.qwary.com" path:@"/form/3Y6A066rNaDrV17TDvQBN6VHVe0P5jrXTClr9qVwer0=" params:@{@"email" : @"jondoe@acmeinc.com", @"planId" : @"trial1"}];
+
+  QWSurveyViewController * controller = [[QWSurveyViewController alloc] initWithRequest:request delegate:self];
+
+  UIViewController * root = [UIApplication sharedApplication].keyWindow.rootViewController;
+  [root presentViewController:controller animated:TRUE completion:nil];
+
 }
 
 @end
